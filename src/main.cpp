158b5a58ee0d9b952023534da296587408d812a7
PopsicleYT <gerbitravis@gmail.com>
2026-02-08 16:23:55 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,42 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
 
 using namespace geode::prelude;
 
+$on_mod(Loaded) {
+    log::info("=================================");
+    log::info("GProxy mod loaded!");
+    log::info("=================================");
+    
+    Notification::create("GProxy Loaded!", NotificationIcon::Success)->show();
+}
 
-class $modify(GJBaseGameLayer) {
-	
+#include <Geode/modify/MenuLayer.hpp>
+
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+        
+        log::info("MenuLayer initialized with GProxy active");
+        
+        return true;
+    }
 };
 
+#include <Geode/binding/CCHttpClient.hpp>
 
-#include <Geode/modify/CCHttpClient.hpp>
-
-class $modify(MyHttpClient, cocos2d::extension::CCHttpClient) {
-	void send(cocos2d::extension::CCHttpRequest* request) {
-		
-		std::string originalUrl = request->getUrl();
-		
-		
-		if (originalUrl.find("boomlings.com") != std::string::npos || 
-		    originalUrl.find("robtopgames.com") != std::string::npos ||
-		    originalUrl.find("geometrydashfiles.b-cdn.net") != std::string::npos) {
-			
-			
-			std::string newUrl = "https://the.goon.lat/lumi/nal/" + originalUrl;
-			
-			
-			request->setUrl(newUrl.c_str());
-			
-			
-			log::info("Redirected: {} -> {}", originalUrl, newUrl);
-		}
-		
-		
-		CCHttpClient::send(request);
-	}
-};
-
-
-#include <Geode/modify/GameLevelManager.hpp>
-
-class $modify(MyGameLevelManager, GameLevelManager) {
-	void ProcessHttpRequest(gd::string endpoint, gd::string params, gd::string tag, GJHttpType httpType) {
-		
-		std::string originalUrl = std::string(endpoint);
-		
-		
-		if (originalUrl.find("boomlings.com") != std::string::npos || 
-		    originalUrl.find("robtopgames.com") != std::string::npos) {
-			
-			
-			std::string newUrl = "https://the.goon.lat/lumi/nal/" + originalUrl;
-			endpoint = newUrl;
-			
-			log::info("GameLevelManager redirect: {} -> {}", originalUrl, newUrl);
-		}
-		
-		
-		GameLevelManager::ProcessHttpRequest(endpoint, params, tag, httpType);
-	}
+class $modify(MyHttpClient, CCHttpClient) {
+    void send(CCHttpRequest* request) {
+        std::string url = request->getUrl();
+        log::info("HTTP Request to: {}", url);
+        
+        if (url.find("boomlings") != std::string::npos || 
+            url.find("robtopgames") != std::string::npos) {
+            
+            std::string newUrl = "https://the.goon.lat/lumi/nal/" + url;
+            request->setUrl(newUrl.c_str());
+            log::info("REDIRECTED TO: {}", newUrl);
+        }
+        
+        CCHttpClient::send(request);
+    }
 };
