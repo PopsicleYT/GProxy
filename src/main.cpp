2c6e5a2c77dc962f31d473fad1bb8323d583910c
PopsicleYT <gerbitravis@gmail.com>
2026-02-08 16:29:09 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,42 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/GameLevelManager.hpp>
 
 using namespace geode::prelude;
 
+const std::string PROXY_BASE = "https://the.goon.lat/lumi/nal/";
+
 $on_mod(Loaded) {
     log::info("=================================");
-    log::info("GProxy mod loaded!");
+    log::info("GProxy mod loaded successfully!");
+    log::info("Proxy: {}", PROXY_BASE);
     log::info("=================================");
-    
-    Notification::create("GProxy Loaded!", NotificationIcon::Success)->show();
 }
 
-#include <Geode/modify/MenuLayer.hpp>
-
-class $modify(MyMenuLayer, MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
+class $modify(MyGameLevelManager, GameLevelManager) {
+    
+    void ProcessHttpRequest(gd::string endpoint, gd::string params, gd::string tag, GJHttpType httpType) {
+        std::string originalUrl = std::string(endpoint);
+        log::info("ProcessHttpRequest called with: {}", originalUrl);
         
-        log::info("MenuLayer initialized with GProxy active");
-        
-        return true;
-    }
-};
-
-#include <Geode/binding/CCHttpClient.hpp>
-
-class $modify(MyHttpClient, CCHttpClient) {
-    void send(CCHttpRequest* request) {
-        std::string url = request->getUrl();
-        log::info("HTTP Request to: {}", url);
-        
-        if (url.find("boomlings") != std::string::npos || 
-            url.find("robtopgames") != std::string::npos) {
+        if (originalUrl.find("boomlings.com") != std::string::npos || 
+            originalUrl.find("robtopgames.com") != std::string::npos) {
             
-            std::string newUrl = "https://the.goon.lat/lumi/nal/" + url;
-            request->setUrl(newUrl.c_str());
-            log::info("REDIRECTED TO: {}", newUrl);
+            std::string newUrl = PROXY_BASE + originalUrl;
+            endpoint = newUrl;
+            
+            log::info("REDIRECTED: {} -> {}", originalUrl, newUrl);
         }
         
-        CCHttpClient::send(request);
+        GameLevelManager::ProcessHttpRequest(endpoint, params, tag, httpType);
+    }
+    
+    void getOnlineLevels(GJSearchObject* searchObj) {
+        log::info("getOnlineLevels called!");
+        GameLevelManager::getOnlineLevels(searchObj);
+    }
+    
+    void downloadLevel(int levelID, bool unknown) {
+        log::info("downloadLevel called for ID: {}", levelID);
+        GameLevelManager::downloadLevel(levelID, unknown);
     }
 };
