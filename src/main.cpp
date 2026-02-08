b4f74f9be0812255b4e3f1f664f50c86c3327956
PopsicleYT <gerbitravis@gmail.com>
2026-02-08 16:57:01 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,53 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GameLevelManager.hpp>
+#include <smjs.gdintercept/proxy/Proxy.hpp>
 
 using namespace geode::prelude;
+using namespace proxy::prelude;
 
 const std::string PROXY_BASE = "https://the.goon.lat/lumi/nal/";
 
-$on_mod(Loaded) {
-    log::info("=================================");
-    log::info("GProxy mod loaded successfully!");
-    log::info("Proxy: {}", PROXY_BASE);
-    log::info("=================================");
+std::string urlEncode(const std::string& value) {
+    std::ostringstream escaped;
+    escaped.fill('0');
+    escaped << std::hex;
+
+    for (char c : value) {
+        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+            escaped << c;
+            continue;
+        }
+
+        escaped << std::uppercase;
+        escaped << '%' << std::setw(2) << int((unsigned char) c);
+        escaped << std::nouppercase;
+    }
+
+    return escaped.str();
 }
 
-class $modify(MyGameLevelManager, GameLevelManager) {
-    
-    void ProcessHttpRequest(gd::string endpoint, gd::string params, gd::string tag, GJHttpType httpType) {
-        std::string originalUrl = std::string(endpoint);
-        log::info("ProcessHttpRequest called with: {}", originalUrl);
-        
+$execute {
+    log::info("=================================");
+    log::info("GProxy URL Redirector loaded!");
+    log::info("All GD server requests will be proxied to:");
+    log::info("{}", PROXY_BASE);
+    log::info("=================================");
+
+    new EventListener([](RequestEvent* event) {
+        auto& request = event->getRequest();
+        std::string originalUrl = request.getURL().getRaw();
+
         if (originalUrl.find("boomlings.com") != std::string::npos || 
-            originalUrl.find("robtopgames.com") != std::string::npos) {
+            originalUrl.find("robtopgames.com") != std::string::npos ||
+            originalUrl.find("geometrydashfiles.b-cdn.net") != std::string::npos) {
+
+            std::string encodedUrl = urlEncode(originalUrl);
+            std::string newUrl = PROXY_BASE + encodedUrl;
             
-            std::string newUrl = PROXY_BASE + originalUrl;
-            endpoint = newUrl;
+            request.setURL(newUrl);
             
             log::info("REDIRECTED: {} -> {}", originalUrl, newUrl);
         }
         
-        GameLevelManager::ProcessHttpRequest(endpoint, params, tag, httpType);
-    }
-    
-    void getOnlineLevels(GJSearchObject* searchObj) {
-        log::info("getOnlineLevels called!");
-        GameLevelManager::getOnlineLevels(searchObj);
-    }
-};
+        return ListenerResult::Propagate;
+    }, RequestFilter());
+}
